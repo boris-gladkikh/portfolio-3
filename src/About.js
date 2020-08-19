dbe51b0c5c1ba184a8ed93ed39cca7f482503a47
gladkill <gladkillmusic@gmail.com>
2020-08-18 23:51:48 -0700

    updated contact and about pages with more dynamic headings and css styling.

--- src/About.js
+++ src/About.js
@@ -11,23 +11,19 @@ function About() {
           <img src={profilePic} alt="Boris pic" className="profile-pic" />
         </div>
         <div className="m-auto px-5 mb-5 bio-text">
-          <h2 className="mt-5">HELLO! I'M BORIS.</h2>
-          <p style={{ fontSize: "18px", color: "white" }}>
+          <h2 className="hello-text mt-5">HELLO! I'M BORIS.</h2>
+
+          <h3 className="mt-5 ">
             I am a Russian born, NY raised software engineer now residing on the west coast.<br />
-            I specialize in full stack development with an emphasis on front end design and optimization.</p><br />
-          <p className="m-auto mt-5">
-            Software engineering is both a career and a passion for me. From a unique and creative approach towards problem
-            solving, to intuitive design and intelligent layout, software development has proven to be a rewarding and exciting
-            experience. I constantly strive to learn new things and increase my skillset every day.<br />
-            When I'm not coding I spend my time playing piano, writing and recording music, cooking and loving food
-            of all kinds, reading sci-fi and horror, playing games (videogames, tabletop, and board games), watching cinema and
-            shows (let's talk about Watchmen!) and hanging out with my cat and dog.
-        </p>
+          </h3>
+          <h5 className="my-5 light-blue-text">
+            I specialize in full stack development with an emphasis on front end design and optimization.
+            </h5>
         </div>
       </div>
 
       <div className="px-5 py-5 teal-box text-center">
-        <div className="mb-5">
+        <div className="my-5">
           <h3>TOOLKIT</h3>
           <h5>
             <b>
@@ -41,9 +37,25 @@ function About() {
           </b>
           </h5>
         </div>
-
         <ToolkitIcons />
+      </div>
 
+      <div className="bluer-box text-center">
+        <h3 className="py-5 light-blue-text alata-font">
+          SOFTWARE DEVELOPMENT IS MY PASSION.          </h3>
+        <p className="m-auto px-5 bio-text text-white">
+          From a unique and creative approach towards problem
+          solving, to intuitive design and intelligent layout, software development continues to captivate and inspire me, and has proven to be a rewarding and exciting
+          career path.
+        </p>
+        <h3 className="alata-font pt-5">
+          WHEN I'M NOT CODING...
+        </h3>
+        <p className="m-auto px-5 pb-5 bio-text">
+          I spend my time playing piano, writing and recording music, cooking and loving food
+          of all kinds, reading sci-fi and horror, playing games (videogames, tabletop, and board games), watching cinema and
+          shows (let's talk about Watchmen!) and hanging out with my cat & dog.
+        </p>
       </div>
 
     </>
--- src/Contact.js
+++ src/Contact.js
@@ -5,27 +5,34 @@ import './Contact.css';
 
 
 function Contact() {
-  return(
+  const resumeLink = "https://github.com/boris-gladkikh/updatedportfolio/blob/master/Boris%20Gladkikh%202020%20Resume.pdf";
+
+
+  return (
     <>
-    <div className="blue-box text-center p-5">
-      <h2 className="pb-5">CONTACT</h2>
-      <div className="bio-text">
-      <h4 className="">I am currently looking for new employment opportunities!</h4>
-      <h6 className="text-white mb-4">Click on the icon below to download my resume.</h6>
-      
-      <a href="https://github.com/boris-gladkikh/updatedportfolio/blob/master/Boris%20Gladkikh%202020%20Resume.pdf"><p><i><i className="text-white fa-4x far fa-file"></i></i></p></a>
-      <p className="mt-5">
-      For project inquiries, possible employment, collaborative endeavors, etc. please E-mail 
-      me, use the provided contact form, or find me at any of the social media links below.
+      <div className="blue-box text-center p-5">
+        <h2 className="pb-5">CONTACT</h2>
+        <div className="bio-text">
+          <h4 className="alata-font light-blue-text pb-2">
+            I AM CURRENTLY LOOKING FOR NEW EMPLOYMENT OPPORTUNITIES.
+          </h4>
+
+          <h6 className="text-white mb-4">Click on the icon below to download my resume.</h6>
+          <a href={resumeLink}>
+            <i className="text-white fa-4x far fa-file"></i>
+          </a>
+          <p className="mt-5">
+            For inquiries, offers, collaborative endeavors, etc. please E-mail
+            me, use the provided contact form, or find me at any of the social media links below.
       </p>
+        </div>
+        <ContactIcons />
       </div>
-      <ContactIcons />
-    </div>
-    <div className="text-center text-white teal-box p-5 ">
-      <h2 className="mb-5">I'd love to hear from you.</h2>
-      <ContactForm />
-    
-    </div>
+      <div className="text-center text-white teal-box p-5 ">
+        <h2 className="mb-5 alata-font">I'D LOVE TO HEAR FROM YOU.</h2>
+        <ContactForm />
+
+      </div>
     </>
   )
 }
