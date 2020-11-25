d81896917d464d6589e07b077617bd64ebd98527
gladkill <gladkillmusic@gmail.com>
2020-11-25 12:27:44 -0800

    updated resume link to new resume

--- src/Toolkit.js
+++ src/Toolkit.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './Toolkit.css';
-const resumeLink = "https://github.com/boris-gladkikh/updatedportfolio/raw/master/Boris%20Gladkikh%202020%20Resume.pdf";
-// const resumeLonk = "https://github.com/boris-gladkikh/updatedportfolio/raw/master/Boris%20Gladkikh%202020%20Resume.pdf";
+const resumeLink = "https://github.com/boris-gladkikh/portfolio-3/raw/master/Boris%20Gladkikh%20Front%20End%20Engineer%20Resume.pdf";
 
 let programsArray = [
   "Javascript", "React", "Redux", "Node",  "ExpressJS",
